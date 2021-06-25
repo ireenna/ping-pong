2371e45ebb059fbb24b7f251befc02db19a8f155
Irina <irina.kbts@gmail.com>
2021-06-25 17:17:52 +0300

    Second commit

--- Pinger2.0/Program.cs
+++ Pinger2.0/Program.cs
@@ -15,6 +15,7 @@ namespace Pinger2._0
             IMessageConsumerScopeFactory consumer1 = new MessageConsumerScopeFactory(connect);
             QueueService qs = new QueueService(producer1, consumer1);
             MessageService ms = new MessageService(producer1, consumer1);
+            MessageService ms1 = new MessageService(producer1, consumer1);
             qs.Ping("Ping "+ DateTime.Now.ToString());
         }
     }
